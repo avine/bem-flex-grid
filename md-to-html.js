a87870b9f3a9a23647d008259002b922a611fb7f
Stéphane Francel <contact@avine.io>
2018-09-16 05:26:18 +0200

    Add build directory and a watch mode for md-to-html script

--- md-to-html.js
+++ md-to-html.js
@@ -1,16 +1,21 @@
 /**
- * Run this script (using npm script: `npm run md-to-html`) each time you change the file `README.md`
+ * Run this script using npm script: `npm run md-to-html` each time you change the file `README.md`.
+ * To run this script in watch mode, use: `npm run md-to-html watch`.
  */
 
 const Fs = require('fs');
 const Path = require('path');
-
-const markdown = Fs.readFileSync(Path.join(__dirname, './README.md'), { encoding: 'utf-8' });
-
 const MarkdownIt = require('markdown-it');
+
 const md = new MarkdownIt();
 
-const html = `<!DOCTYPE html>
+const sourcePath = Path.join(__dirname, './README.md');
+const targetPath = Path.join(__dirname, './src/introduction.html');
+
+const mdToHtml = () => {
+  const markdown = Fs.readFileSync(sourcePath, { encoding: 'utf-8' });
+
+  const html = `<!DOCTYPE html>
 <html lang="en" id="demo-index">
 <head>
   <meta charset="utf-8">
@@ -27,4 +32,13 @@ ${md.render(markdown)}
 </body>
 </html>`;
 
-Fs.writeFileSync(Path.join(__dirname, './src/introduction.html'), html, { encoding: 'utf-8' });
+  Fs.writeFileSync(targetPath, html, { encoding: 'utf-8' });
+  console.log('Updating:', targetPath);
+};
+
+mdToHtml();
+
+if (process.argv[2] === 'watch') {
+  Fs.watchFile(sourcePath, mdToHtml);
+  console.log('watching:', sourcePath);
+}
